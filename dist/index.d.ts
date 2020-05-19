1a6ec9841e9e28b108b377bd2d5f2ae18a6e9a1e
SaltyAom <aomkirby@mystiar.com>
2020-05-19 14:25:43 +0700

    :wrench: Remove dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import ProgressiveHydrationComponent from './types';
-export declare const removeEvent: (ReactComponent: any) => any;
-declare const ProgressiveHydration: ProgressiveHydrationComponent;
-export default ProgressiveHydration;
